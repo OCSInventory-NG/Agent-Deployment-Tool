9ea2dcbdc3c9a8259d3a282bedad737682aa013a
dliroulet <dliroulet@7652ea07-a258-4d52-b9d5-dd0e67ff0423>
2008-04-01 21:16:47 +0000

    Initial NSIS setup script (requires putty.exe, pscp.exe and remcom.exe)

---
+++ Setup/setup.nsi
@@ -0,0 +1,158 @@
+################################################################################
+## OCS Inventory NG
+## Copyleft 2008 OCS Inventory NG Team
+## Web : http://www.ocsinventory-ng.org
+##
+## This code is open source and may be copied and modified as long as the source
+## code is always made freely available.
+## Please refer to the General Public Licence http://www.gnu.org/ or Licence.txt
+################################################################################
+
+; HM NIS Edit Wizard helper defines
+!define PRODUCT_NAME "OCS Inventory NG Agent Deployment Tool"
+!define PRODUCT_VERSION "1.0.1.1"
+!define PRODUCT_PUBLISHER "OCS Inventory NG"
+!define PRODUCT_WEB_SITE "http://www.ocsinventory-ng.org"
+!define PRODUCT_DIR_REGKEY "Software\Microsoft\Windows\CurrentVersion\App Paths\OCS_DEPLOY_TOOL.exe"
+!define PRODUCT_UNINST_KEY "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PRODUCT_NAME}"
+!define PRODUCT_UNINST_ROOT_KEY "HKLM"
+!define PRODUCT_STARTMENU_REGVAL "NSIS:StartMenuDir"
+
+SetCompressor bzip2
+
+; MUI 1.67 compatible ------
+!include "MUI.nsh"
+
+; MUI Settings
+!define MUI_ABORTWARNING
+!define MUI_ICON "..\..\win32_agent\service_agent_setup\aocs2.ico"
+!define MUI_UNICON "..\..\win32_agent\service_agent_setup\uocs2.ico"
+!define MUI_HEADERIMAGE_BITMAP "..\..\win32_agent\service_agent_setup\lOCS-ng-48.bmp" ; optional
+
+!define MUI_HEADERIMAGE
+!define MUI_WELCOMEPAGE_TITLE_3LINES
+; Welcome page
+!insertmacro MUI_PAGE_WELCOME
+; License page
+!define MUI_LICENSEPAGE_CHECKBOX
+!insertmacro MUI_PAGE_LICENSE "LICENSE.txt"
+; Directory page
+!insertmacro MUI_PAGE_DIRECTORY
+; Start menu page
+var ICONS_GROUP
+!define MUI_STARTMENUPAGE_NODISABLE
+!define MUI_STARTMENUPAGE_DEFAULTFOLDER "OCS Inventory NG Agent Deployment Tool"
+!define MUI_STARTMENUPAGE_REGISTRY_ROOT "${PRODUCT_UNINST_ROOT_KEY}"
+!define MUI_STARTMENUPAGE_REGISTRY_KEY "${PRODUCT_UNINST_KEY}"
+!define MUI_STARTMENUPAGE_REGISTRY_VALUENAME "${PRODUCT_STARTMENU_REGVAL}"
+!insertmacro MUI_PAGE_STARTMENU Application $ICONS_GROUP
+; Instfiles page
+!insertmacro MUI_PAGE_INSTFILES
+; Finish page
+!define MUI_FINISHPAGE_RUN "$INSTDIR\OCS_DEPLOY_TOOL.exe"
+!define MUI_FINISHPAGE_RUN_TEXT "Start ${PRODUCT_NAME}"
+!define MUI_FINISHPAGE_TITLE_3LINES
+!insertmacro MUI_PAGE_FINISH
+
+; Uninstaller pages
+!insertmacro MUI_UNPAGE_INSTFILES
+
+; Language files
+!insertmacro MUI_LANGUAGE "English"
+!insertmacro MUI_LANGUAGE "French"
+
+; Reserve files
+!insertmacro MUI_RESERVEFILE_INSTALLOPTIONS
+
+; MUI end ------
+
+  VIProductVersion "${PRODUCT_VERSION}"
+  VIAddVersionKey /LANG=${LANG_ENGLISH} "ProductName" "${PRODUCT_NAME}"
+  VIAddVersionKey /LANG=${LANG_ENGLISH} "Comments" "Help deploying OCS Inventory NG Agent under Windows and Unix computers."
+  VIAddVersionKey /LANG=${LANG_ENGLISH} "CompanyName" "${PRODUCT_PUBLISHER}"
+  VIAddVersionKey /LANG=${LANG_ENGLISH} "LegalTrademarks" "${PRODUCT_NAME} is a part of OCS Inventory NG product. Distributed under GNU GPL Licence."
+  VIAddVersionKey /LANG=${LANG_ENGLISH} "LegalCopyright" "${PRODUCT_PUBLISHER} ${PRODUCT_WEB_SITE}"
+  VIAddVersionKey /LANG=${LANG_ENGLISH} "FileDescription" "${PRODUCT_NAME}"
+  VIAddVersionKey /LANG=${LANG_ENGLISH} "FileVersion" "${PRODUCT_VERSION}"
+
+BRANDINGTEXT "OCS Inventory NG"
+Name "${PRODUCT_NAME} ${PRODUCT_VERSION}"
+OutFile "OCSNG_Agent_Deployment_Tool_Setup.exe"
+InstallDir "$PROGRAMFILES\OCS Inventory NG Agent Deployment Tool"
+InstallDirRegKey HKLM "${PRODUCT_DIR_REGKEY}" ""
+ShowInstDetails hide
+ShowUnInstDetails hide
+
+Section "Main Files" SEC01
+  SetOutPath "$INSTDIR"
+  SetOverwrite ifnewer
+  File "..\Release\OCS_DEPLOY_TOOL.exe"
+  File "modules.conf"
+  File "pscp.exe"
+  File "putty.exe"
+  File "RemCom.exe"
+  File "setup.sh"
+  File "LICENSE.txt"
+  File "PuTTY_LICENCE.txt"
+
+; Shortcuts
+  !insertmacro MUI_STARTMENU_WRITE_BEGIN Application
+  CreateDirectory "$SMPROGRAMS\$ICONS_GROUP"
+  CreateShortCut "$SMPROGRAMS\$ICONS_GROUP\OCS Inventory NG Agent Deployment Tool.lnk" "$INSTDIR\OCS_DEPLOY_TOOL.exe"
+  CreateShortCut "$DESKTOP\OCS Inventory NG Agent Deployment Tool.lnk" "$INSTDIR\OCS_DEPLOY_TOOL.exe"
+  !insertmacro MUI_STARTMENU_WRITE_END
+SectionEnd
+
+Section -AdditionalIcons
+  !insertmacro MUI_STARTMENU_WRITE_BEGIN Application
+  WriteIniStr "$INSTDIR\${PRODUCT_PUBLISHER}.url" "InternetShortcut" "URL" "${PRODUCT_WEB_SITE}"
+  CreateShortCut "$SMPROGRAMS\$ICONS_GROUP\OCS Inventory NG Website.lnk" "$INSTDIR\${PRODUCT_PUBLISHER}.url"
+  CreateShortCut "$SMPROGRAMS\$ICONS_GROUP\Uninstall.lnk" "$INSTDIR\uninst.exe"
+  !insertmacro MUI_STARTMENU_WRITE_END
+SectionEnd
+
+Section -Post
+  WriteUninstaller "$INSTDIR\uninst.exe"
+  WriteRegStr HKLM "${PRODUCT_DIR_REGKEY}" "" "$INSTDIR\OCS_DEPLOY_TOOL.exe"
+  WriteRegStr ${PRODUCT_UNINST_ROOT_KEY} "${PRODUCT_UNINST_KEY}" "DisplayName" "$(^Name)"
+  WriteRegStr ${PRODUCT_UNINST_ROOT_KEY} "${PRODUCT_UNINST_KEY}" "UninstallString" "$INSTDIR\uninst.exe"
+  WriteRegStr ${PRODUCT_UNINST_ROOT_KEY} "${PRODUCT_UNINST_KEY}" "DisplayIcon" "$INSTDIR\OCS_DEPLOY_TOOL.exe"
+  WriteRegStr ${PRODUCT_UNINST_ROOT_KEY} "${PRODUCT_UNINST_KEY}" "DisplayVersion" "${PRODUCT_VERSION}"
+  WriteRegStr ${PRODUCT_UNINST_ROOT_KEY} "${PRODUCT_UNINST_KEY}" "URLInfoAbout" "${PRODUCT_WEB_SITE}"
+  WriteRegStr ${PRODUCT_UNINST_ROOT_KEY} "${PRODUCT_UNINST_KEY}" "Publisher" "${PRODUCT_PUBLISHER}"
+SectionEnd
+
+
+Function un.onUninstSuccess
+  HideWindow
+  MessageBox MB_ICONINFORMATION|MB_OK "$(^Name) was successfully uninstalled from your computer."
+FunctionEnd
+
+Function un.onInit
+  MessageBox MB_ICONQUESTION|MB_YESNO|MB_DEFBUTTON2 "Do you really want to uninstall $(^Name) ?" IDYES +2
+  Abort
+FunctionEnd
+
+Section Uninstall
+  !insertmacro MUI_STARTMENU_GETFOLDER "Application" $ICONS_GROUP
+  Delete "$INSTDIR\${PRODUCT_NAME}.url"
+  Delete "$INSTDIR\uninst.exe"
+  Delete "$INSTDIR\setup.sh"
+  Delete "$INSTDIR\RemCom.exe"
+  Delete "$INSTDIR\putty.exe"
+  Delete "$INSTDIR\pscp.exe"
+  Delete "$INSTDIR\modules.conf"
+  Delete "$INSTDIR\OCS_DEPLOY_TOOL.exe"
+
+  Delete "$SMPROGRAMS\$ICONS_GROUP\Uninstall.lnk"
+  Delete "$SMPROGRAMS\$ICONS_GROUP\OCS Inventory NG Website.lnk"
+  Delete "$DESKTOP\OCS Inventory NG Agent Deployment Tool.lnk"
+  Delete "$SMPROGRAMS\$ICONS_GROUP\OCS Inventory NG Agent Deployment Tool.lnk"
+
+  RMDir "$SMPROGRAMS\$ICONS_GROUP"
+  RMDir "$INSTDIR"
+
+  DeleteRegKey ${PRODUCT_UNINST_ROOT_KEY} "${PRODUCT_UNINST_KEY}"
+  DeleteRegKey HKLM "${PRODUCT_DIR_REGKEY}"
+  SetAutoClose true
+SectionEnd
---
+++ Setup/setup.sh
@@ -0,0 +1,234 @@
+#!/bin/sh
+###############################################################################
+#
+# OCS Inventory NG Unified Unix Agent Setup script
+#
+# Copyleft 2008 OCS Inventory NG Team
+# http://www.ocsinventory-ng.org
+###############################################################################
+#
+# Usage:
+#     setup.sh AGENT_BASE_FILENAME SERVER_ADDRESS[:PORT] [CONFIG_DIR] [VAR_DIR]
+#
+# AGENT_BASE_FILENAME = Ocsinventory-Agent-version (without .tar.gz)
+# SERVER_ADDRESS = 192.168.1.25:80 for example
+# CONFIG_DIR = global agent configuration directory (optional, default to 
+#              /etc/ocsinnventory-agent)
+# VAR_DIR = global agent var dircetory (optional, default to 
+#           /var/lib/ocsinventory-agent)
+
+###############################################################################
+############################  DO NOT MODIFY BELOW  ############################
+###############################################################################
+
+# Set silent install
+export PERL_AUTOINSTALL=1
+# Tempory directory to use
+TMP_DIR="/tmp"
+# Where to store setup log file
+SETUP_LOG="$TMP_DIR/ocs_agent_setup.log"
+# Agent global configuration file
+CONFIG_FILE="ocsinventory-agent.cfg"
+# Agent global module file
+MODULE_FILE="modules.conf"
+# Cron task file name and directory
+CRON_TASK="ocsinventory-agent"
+CRON_DIR="/etc/cron.d"
+
+###############################################################################
+# Setup parameters
+echo > $SETUP_LOG
+OCS_LOCAL_DATE=`date +%Y-%m-%d-%H-%M-%S`
+echo "Starting OCS Inventory NG Agent setup on $OCS_LOCAL_DATE" >> $SETUP_LOG
+echo -n "from folder " >> $SETUP_LOG
+pwd >> $SETUP_LOG
+echo "Storing log in file $SETUP_LOG" >> $SETUP_LOG
+echo >> $SETUP_LOG
+echo "Parsing script parameters <$1> <$2> <$3> <$4>..." >> $SETUP_LOG
+# Setup file base name (must be followed by .tar.gz)
+if [ -z $1 ]
+then
+    echo "*** ERROR: missing first mandatory parameter !" >> $SETUP_LOG
+    exit 1
+else
+    BASE_FILENAME="$1"
+fi
+echo "Using <$BASE_FILENAME.tar.gz> as Agent source file..." >> $SETUP_LOG
+# Server address
+if [ -z $2 ]
+then
+    echo "*** ERROR: missing second mandatory parameter !" >> $SETUP_LOG
+    exit 1
+else
+    SERVER_URI="$2"
+fi
+echo "Using <$SERVER_URI> as server address..." >> $SETUP_LOG
+# Agent global configuration directory
+if [ -z $3 ]
+then
+    CONFIG_DIR="/etc/ocsinventory-agent"
+else
+    CONFIG_DIR=$3
+fi
+echo "Using <$CONFIG_DIR> as Agent global configuration directory..." >> $SETUP_LOG 
+# Agent var dir
+if [ -z $4 ]
+then
+    VAR_DIR="/var/lib/ocsinventory-agent"
+else
+    VAR_DIR=$4
+fi
+echo "Using <$VAR_DIR> as Agent global state directory..." >> $SETUP_LOG 
+
+###############################################################################
+# Agent setup
+echo "Moving to directory $TMP_DIR..." >> $SETUP_LOG
+cd $TMP_DIR >> $SETUP_LOG 2>&1
+if [ $? -ne 0 ]
+then
+    echo "*** ERROR Unable to cd to specified temp folder !" >> $SETUP_LOG
+    exit 1
+fi
+echo "Gunzip Agent archive $BASE_FILENAME.tar.gz..." >> $SETUP_LOG
+gzip -d "$BASE_FILENAME.tar.gz" >> $SETUP_LOG 2>&1
+if [ $? -ne 0 ]
+then
+    echo "*** ERROR Unable to gunzip Agent archive !" >> $SETUP_LOG
+    exit 1
+fi
+echo "Untar Agent archive $BASE_FILENAME.tar..." >> $SETUP_LOG
+tar -xf "$BASE_FILENAME.tar" >> $SETUP_LOG 2>&1
+if [ $? -ne 0 ]
+then
+    echo "*** ERROR Unable to untar Agent archive !" >> $SETUP_LOG
+    exit 1
+fi
+echo "Entering Agent sub directory $BASE_FILENAME..." >> $SETUP_LOG
+cd "$BASE_FILENAME" >> $SETUP_LOG 2>&1
+if [ $? -ne 0 ]
+then
+    echo "*** ERROR Unable to enter Agent sub directory !" >> $SETUP_LOG
+    exit 1
+fi
+echo "Configuring Agent (perl Makefile.PL)..." >> $SETUP_LOG
+perl Makefile.PL >> $SETUP_LOG 2>&1
+if [ $? -ne 0 ]
+then
+    echo "*** ERROR Unable to configure Agent !" >> $SETUP_LOG
+    exit 1
+fi
+echo "Building Agent (make)..." >> $SETUP_LOG
+make >> /dev/null 2>>$SETUP_LOG
+if [ $? -ne 0 ]
+then
+    echo "*** ERROR Unable to build Agent !" >> $SETUP_LOG
+    exit 1
+fi
+echo "Testing Agent (make test)..." >> $SETUP_LOG
+make test >> /dev/null 2>>$SETUP_LOG
+if [ $? -ne 0 ]
+then
+    echo "*** WARNING Agent test failed !" >> $SETUP_LOG
+fi
+echo "Installing Agent (make install)..." >> $SETUP_LOG
+make install >> /dev/null 2>>$SETUP_LOG
+if [ $? -ne 0 ]
+then
+    echo "*** ERROR Unable to install Agent !" >> $SETUP_LOG
+    exit 1
+fi
+echo "Leaving Agent directory..." >> $SETUP_LOG
+cd .. >> $SETUP_LOG 2>&1
+echo "Removing Agent directory $BASE_FILENAME..." >> $SETUP_LOG
+rm -Rf "$BASE_FILENAME*" >> $SETUP_LOG 2>&1
+
+
+###############################################################################
+# Agent configurationn
+echo "Creating configuration directories..." >> $SETUP_LOG
+mkdir -p $CONFIG_DIR >> $SETUP_LOG 2>&1
+if [ $? -ne 0 ]
+then
+    echo "*** ERROR Unable to create $CONFIG_DIR directory !" >> $SETUP_LOG
+    exit 1
+fi
+mkdir -p $VAR_DIR/$SERVER_URI >> $SETUP_LOG 2>&1
+if [ $? -ne 0 ]
+then
+    echo "*** ERROR Unable to create $VAR_DIR/$SERVER_URI directory !" >> $SETUP_LOG
+    exit 1
+fi
+echo "Creating global configuration files..." >> $SETUP_LOG
+cp $TMP_DIR/$CONFIG_FILE $CONFIG_DIR/$CONFIG_FILE >> $SETUP_LOG 2>&1
+if [ $? -ne 0 ]
+then
+    echo "*** ERROR Unable to copy $TMP_DIR/$CONFIG_FILE to $CONFIG_DIR/$CONFIG_FILE !" >> $SETUP_LOG
+    exit 1
+fi
+cp $TMP_DIR/$MODULE_FILE $CONFIG_DIR/$MODULE_FILE >> $SETUP_LOG 2>&1
+if [ $? -ne 0 ]
+then
+    echo "*** ERROR Unable to copy $TMP_DIR/$MODULE_FILE to $CONFIG_DIR/$MODULE_FILE !" >> $SETUP_LOG
+    exit 1
+fi
+if [ -d $TMP_DIR/$SERVER_URI ]
+then
+    echo "Copying additional configuration files for server $SERVER_URI..." >> $SETUP_LOG
+    cp $TMP_DIR/$SERVER_URI/* $VAR_DIR/$SERVER_URI >> $SETUP_LOG 2>&1
+    if [ $? -ne 0 ]
+    then
+        echo "*** ERROR Unable to copy files from $TMP_DIR/$SERVER_URI to $VAR_DIR/$SERVER_URI !" >> $SETUP_LOG
+        exit 1
+    fi
+    rm -Rf "$TMP_DIR/$SERVER_URI" >> $SETUP_LOG 2>&1
+fi
+echo "Creating cron task..." >> $SETUP_LOG
+if [ -e $CRON_DIR/$CRON_TASK ]
+then
+    echo "Cron task already exists, skipping..." >> $SETUP_LOG
+else
+    CRON_MIN=`date +%M`
+    echo "# Run OCS Inventory NG Agent" > $CRON_DIR/$CRON_TASK
+    echo "$CRON_MIN * * * * root /usr/bin/ocsinventory-agent --lazy > /dev/null 2>&1" >> $CRON_DIR/$CRON_TASK
+    if [ $? -ne 0 ]
+    then
+        echo "*** ERROR Unable to create $CRON_DIR/$CRON_TASK !" >> $SETUP_LOG
+        exit 1
+    fi
+fi
+
+###############################################################################
+# Old Agent for Linux settings migration
+if [ -e /etc/ocsinventory-client/ocsinv.conf ]
+then
+    echo "Migrating settings from old OCS Inventory NG Agent for Linux (/etc/ocsinventory-client/)..." >> $SETUP_LOG
+    cp -Rf /etc/ocsinventory-client/* $VAR_DIR/$SERVER_URI >> $SETUP_LOG 2>&1
+    if [ $? -ne 0 ]
+    then
+        echo "*** ERROR Unable to copy files from /etc/ocsinventor-client/ to $VAR_DIR/$SERVER_URI !" >> $SETUP_LOG
+        exit 1
+    fi
+    rm -Rf /etc/ocsinventory-client >> $SETUP_LOG 2>&1
+    if [ -e /bin/ocsinv ]
+    then
+        rm -f /bin/ocsinv >> $SETUP_LOG 2>&1
+    fi
+    if [ -e /usr/sbin/ocsinventory-client.pl ]
+    then
+        rm -f /usr/sbin/ocsinventory-client.pl >> $SETUP_LOG 2>&1
+    fi
+    if [ -e /etc/logrotate.d/ocsinventory-client ]
+    then
+        rm -f /etc/logrotate.d/ocsinventory-client >> $SETUP_LOG 2>&1
+    fi
+    if [ -e /etc/cron.d/ocsinventory-client ]
+    then
+        rm -f /etc/cron.d/ocsinventory-client >> $SETUP_LOG 2>&1
+    fi
+fi
+
+###############################################################################
+# SUCCESS
+echo "SUCCESS installing OCS Inventory NG Agent ;-)" >> $SETUP_LOG
+exit 0
+
