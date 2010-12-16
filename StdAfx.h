9e715adead3d5c69b2041b0619bd4ff3ad5c3103
dliroulet <didier.liroulet@ocsnventory-ng.org>
2010-12-16 17:39:03 +0100

    Updates for MS VC++ 2008 libs and unicode build

--- StdAfx.h
+++ StdAfx.h
@@ -12,30 +12,54 @@
 //  or project specific include files that are used frequently, but
 //      are changed infrequently
 //
-
-#if !defined(AFX_STDAFX_H__EB7282E2_5CC6_4C25_A859_7F2981E3F7D2__INCLUDED_)
-#define AFX_STDAFX_H__EB7282E2_5CC6_4C25_A859_7F2981E3F7D2__INCLUDED_
-
-#if _MSC_VER > 1000
 #pragma once
-#endif // _MSC_VER > 1000
 
+#ifndef _SECURE_ATL
+#define _SECURE_ATL 1
+#endif
+
+#ifndef VC_EXTRALEAN
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+#endif
+
+// The following define the minimum required platform.  The minimum required platform is the 
+// earliest version of Windows, Internet Explorer etc. that has the necessary features to run 
+// the application.
+#include "targetver.h"
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// some CString constructors will be explicit
+
+// turns off MFC's hiding of some common and often safely ignored warning messages
+#define _AFX_ALL_WARNINGS
 
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
-#include <afxdisp.h>        // MFC Automation classes
+
+
+
+
+
+#ifndef _AFX_NO_OLE_SUPPORT
 #include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
+#endif
 #ifndef _AFX_NO_AFXCMN_SUPPORT
 #include <afxcmn.h>			// MFC support for Windows Common Controls
-#include <afxtempl.h>		// MFC template support
-#include <afxcoll.h>		// MFC collection
-#include <afxsock.h>		// MFC socket support
-
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
 
-//{{AFX_INSERT_LOCATION}}
-// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+	
 
-#endif // !defined(AFX_STDAFX_H__EB7282E2_5CC6_4C25_A859_7F2981E3F7D2__INCLUDED_)
+#include <afxsock.h>		// MFC socket support
+
+
+#ifdef _UNICODE
+#if defined _M_IX86
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_IA64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_X64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+#endif
